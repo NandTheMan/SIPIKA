cb0fbcab1847c6f5f47c520cd0963f32e51c1170
NandTheMan <anandawirasena04@gmail.com>
2024-11-26 00:34:18 +0800

    tambah react lagi

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Head, Link } from '@inertiajs/react';
+
+export default function Homepage(props) {
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    )
+}
